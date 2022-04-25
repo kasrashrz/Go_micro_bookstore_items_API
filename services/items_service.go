a4e674604fff27af22c92b20969eadc55326321f
kasra shirazi <kasra.shirazi.1234@gmail.com>
2022-04-25 20:41:54 +0430

    services

--- services/items_service.go
+++ services/items_service.go
@@ -1,1 +1,21 @@
 package services
+
+import (
+	"github.com/kasrashrz/Go_micro_bookstore_OAth-go/oath/errors"
+	"github.com/kasrashrz/Go_micro_bookstore_items_API/domain/items"
+)
+
+type itemsServiceInterface interface {
+	Create(items.Item) (*items.Item, *errors.RestErr)
+	Get(string) (*items.Item, *errors.RestErr)
+}
+
+type itemsService struct{}
+
+func (service itemsService) Create(item items.Item) (*items.Item, *errors.RestErr) {
+	return nil, nil
+}
+
+func (service itemsService) Get(itemName string) (*items.Item, **errors.RestErr) {
+	return nil, nil
+}
